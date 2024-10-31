70231324908fdfb2baa61b7c6c51c47da9efbcb8
MARIA CLARA CARNEIRO CONTIERO <maria.contiero@puc-campinas.edu.br>
2024-10-31 16:30:17 -0300

    Uns paranaue na pag de modulos

---
+++ flask/static/js/modulo.js
@@ -0,0 +1,22 @@
+const modulos = document.querySelectorAll(".modulo-principal");
+
+//o foreach serve para percorrer listas com elementos
+//nesse caso, dentro do elemento modulos, ele vai percorrer cada classe que for igual a .conteudo e .toggle
+// (elemento atual sendo processado, index => {}
+modulos.forEach(modulo => {
+    const conteudo = modulo.querySelector(".conteudos");
+    const toggle = modulo.querySelector(".toggle");
+
+    conteudo.style.display = "none"; 
+
+//e aqui aplicamos essas propriedades css em cada elemento modulo, contido em modulos
+    modulo.addEventListener("click", function () {
+        if (conteudo.style.display === "none") {
+            toggle.style.transform = "rotate(90deg)";
+            conteudo.style.display = "flex";
+        } else {
+            toggle.style.transform = "rotate(0deg)";
+            conteudo.style.display = "none";
+        }
+    });
+});
