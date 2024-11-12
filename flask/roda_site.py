8a322ba4708d83c5dc2d21ca97668bc1230145ba
MARIA CLARA CARNEIRO CONTIERO <maria.contiero@puc-campinas.edu.br>
2024-11-12 16:58:22 -0300

    fiz a tela de conteudo certa com o flask

--- flask/roda_site.py
+++ flask/roda_site.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DateField, EmailField, PasswordField
-from wtforms.validators import DataRequired 
+from wtforms import StringField, SubmitField, DateField, EmailField, PasswordField, TextAreaField, FileField, RadioField
+from wtforms.validators import DataRequired, regexp
+from wtforms import validators
+
 
 
 ############################################## COMANDO PARA RODAR O SITE ##############################################
@@ -70,9 +72,20 @@ def modulos():
     return render_template('modulos.html')
 
 # Página de conteúdos ##############################################
+class elementos(FlaskForm):
+    h1 = TextAreaField(u'Digite um título...', [validators.optional(), validators.length(max=200)], render_kw={"placeholder": "Digite o título aqui..."})
+    h2 = TextAreaField(u'Digite um subtítulo...', [validators.optional(), validators.length(max=250)], render_kw={"placeholder": "Digite o subtítulo aqui..."})
+    paragrafo = TextAreaField(u'Digite um texto...', [validators.optional(), validators.length(max=1000)], render_kw={"placeholder": "Digite o texto aqui..."})
+    imagem = FileField(u'Imagem', [regexp(r'^[a-zA-Z0-9_-]+\.jpg$', message="Apenas arquivos .jpg são permitidos.")])
+
+
 @app.route("/conteudos")
 def conteudos():
-    return render_template('conteudos.html')
+    name = None
+    form = elementos()
+    return render_template('conteudos.html',
+        name = name,
+        form = form)
 
 # Página de digitação ##############################################
 @app.route("/teste-digitacao")
