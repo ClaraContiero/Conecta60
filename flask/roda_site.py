a99a017c8cc0385f92364f926c7e0d155493a276
MARIA CLARA CARNEIRO CONTIERO <maria.ccc3@puccampinas.edu.br>
2024-11-14 11:14:35 -0300

    banco de dados funcionaaaa

--- flask/roda_site.py
+++ flask/roda_site.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DateField, EmailField, PasswordField, TextAreaField, FileField, RadioField
 from wtforms.validators import DataRequired, regexp
 from wtforms import validators
-
+from werkzeug.security import generate_password_hash
+from extensions import db
 
 
 ############################################## COMANDO PARA RODAR O SITE ##############################################
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    
+    # Configuração da chave secreta e do banco de dados
+    app.config['SECRET_KEY'] = "minhaSenhaHiperUltraMegaBlasterSecreta"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://BD070324136:Ulfea9@BD-ACD/BD070324136"
+    
+    # Inicializar o SQLAlchemy com o app
+    db.init_app(app)
 
-#### CHAVE SECRETA
-app.config['SECRET_KEY'] = "minhaSenhaHiperUltraMegaBlasterSecreta"
+    # hh
+    with app.app_context():
+        db.create_all()
+    
+    return app
+
+app = create_app()
 
 ############################################## ROTAS PARA AS PÁGINAS ##############################################
 
@@ -25,25 +39,58 @@ def cadProfAluno():
     return render_template('cadastroProfAlun.html')
 
 # Página de cadastro ##############################################
+
+class tabela_cadastro(db.Model): 
+    __tablename__ = 'cadastro'
+    id_cadastro = db.Column(db.Integer, primary_key = True)
+    nome_completo = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    data_nasc = db.Column(db.Date, nullable = False)
+    senha = db.Column(db.String(256), nullable = False)
+
 class cadastro(FlaskForm):
-    dataNasc = DateField('Data de Nascimento *', validators=[DataRequired()])
-    nomeCompleto = StringField('Nome Completo *', validators=[DataRequired()])
+    data_nasc = DateField('Data de Nascimento *', validators=[DataRequired()])
+    nome_completo = StringField('Nome Completo *', validators=[DataRequired()])
     email = EmailField('Email *', validators=[DataRequired()])
     senha = PasswordField('Senha *',validators=[DataRequired()])
     enviar = SubmitField('CADASTRAR')
 
 @app.route("/Cadastre-se", methods=['GET', 'POST'])
 def cadastrar():
-    name = None
     form = cadastro()
+    name = None
     #validando o formulario
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        flash('Cadastrado com sucesso!')
-    return render_template('cadastro.html',
-        name = name,
-        form = form)
+        print("Oi")
+        usuario = tabela_cadastro.query.filter_by(email=form.email.data).first()
+        if usuario is None:
+            try:
+
+                senha_hash = generate_password_hash(form.senha.data)
+
+                usuario = tabela_cadastro(
+                    data_nasc = form.data_nasc.data,
+                    nome_completo = form.nome_completo.data,
+                    email = form.email.data,
+                    senha = senha_hash
+                )
+                name = form.nome_completo.data
+                db.session.add(usuario)
+                db.session.commit()
+
+                form.data_nasc.data = ''
+                form.nome_completo.data = ''
+                form.email.data = ''
+                form.senha.data = ''
+
+                flash('Cadastrado com sucesso!')
+                return redirect(url_for('login'))
+            except Exception as e:
+                db.session.rollback()
+                flash(f'Erro ao registrar o usuário: {e}')
+        else:
+                flash('Email já existente.')
+    return render_template('cadastro.html', form = form, name = name)
 
 
 # Página de login ##############################################
